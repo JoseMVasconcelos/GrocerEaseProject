acfc1a99c68a1b9e5a4d36ab63e27577a92188fb
Bernardo De Marco Gonçalves <bernardomg2004@gmail.com>
2023-06-05 20:36:11 -0300

    finished homepage

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,14 +1,17 @@
+import { Link } from 'react-router-dom'
 import styles from './Home.module.css'
 
 export function Home() {
   return (
     <section className={styles.pageContainer}>
-      <div>
-        <h1>Seja bem vindo ao GrocerEase</h1>
-      </div>
+      <h1>Seja bem vindo ao GrocerEase!</h1>
       <div>
         <p>Nunca foi tão fácil fazer a sua lista de compras!</p>
-        <p>Faça o seu login ou cadastre-se para começar!</p>
+        <p>Faça o seu login ou cadastre-se para começar.</p>
+      </div>
+      <div className={styles.buttons}>
+        <Link to={'sign-up'}>Cadastro</Link>
+        <Link to={'sign-in'}>Login</Link>
       </div>
     </section>
   )
