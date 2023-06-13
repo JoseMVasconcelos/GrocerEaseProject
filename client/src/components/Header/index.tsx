e82c3b699c57fbd8fdb32237c9f3d489eaca5fc6
Bernardo De Marco Gonçalves <bernardomg2004@gmail.com>
2023-06-13 15:22:56 -0300

    started EditUserDataModal component

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,14 +1,14 @@
-import logo from './../../assets/logo.png'
 import styles from './Header.module.css'
+import logo from './../../assets/logo.png'
+import { EditUserDataModal } from '../EditUserDataModal'
 
 export function Header() {
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
-        <h2>GrocerEase</h2>
         <img src={logo} alt="" />
       </div>
-      <div className={styles.userInfo}></div>
+      <EditUserDataModal />
     </header>
   )
 }
