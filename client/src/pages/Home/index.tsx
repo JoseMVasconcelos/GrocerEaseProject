c922da0fca1801e4230e43d53bc0507bac9b9abc
Bernardo De Marco Gonçalves <bernardomg2004@gmail.com>
2023-06-25 13:21:20 -0300

    protected home route

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,8 +1,23 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import styles from './Home.module.css'
 import { CustomButton } from '../../components/CustomButton'
+import { useAuthContext } from '../../hooks/useAuthContext'
+import { useEffect } from 'react'
 
 export function Home() {
+  const { isAuthenticated } = useAuthContext()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate('/shoppingLists')
+    }
+  }, [isAuthenticated, navigate])
+
+  if (isAuthenticated) {
+    return <></>
+  }
+
   return (
     <section className={styles.pageContainer}>
       <h1>Seja bem vindo ao GrocerEase!</h1>
